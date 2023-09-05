4679dccd06b2f4af60b724301c2d400736a7358b
naricrow <nari.crow.city@gmail.com>
2023-09-06 00:26:23 +0900

    ログアウト機能の実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,9 +5,9 @@ class UsersController < ApplicationController
 
   def update
     if current_user.update(user_params)
-      redirect_to root_path
+       redirect_to root_path
     else
-      render :edit, status: :unprocessable_entity
+       render :edit, status: :unprocessable_entity
     end
   end
 
